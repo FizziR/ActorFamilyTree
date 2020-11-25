2eb4dcb2380cf3ea63cea1cd96958c6eda6c58ce
FizziR <felicitas@maurerhaus.de>
2020-11-25 10:35:46 +0100

    importing akka-version

--- src/main/scala/helloWorld.scala
+++ src/main/scala/helloWorld.scala
@@ -1,3 +1,21 @@
-class helloWorld {
+import akka.actor.Actor
+import akka.actor.Props
+import akka.event.Logging
+import akka.actor.{ Actor, ActorRef, ActorSystem, PoisonPill, Props }
 
+class MyActor extends Actor {
+  val log = Logging(context.system, this)
+
+  def receive = {
+    case "test" => log.info("received test")
+    case _      => log.info("received unknown message")
+  }
+}
+
+object Main extends App {
+  val system = ActorSystem("pingPong")
+
+  val myActor = system.actorOf(Props[MyActor], name = "myactor")
+  myActor ! "test"
+  myActor ! "hi"
 }
