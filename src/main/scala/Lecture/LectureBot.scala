959cfc4dbc1436cfede6bfbab9279623e5fda969
FizziR <felicitas@maurerhaus.de>
2020-11-28 11:55:08 +0100

    Actors for internal and external DSL and actors

--- src/main/scala/Lecture/LectureBot.scala
+++ src/main/scala/Lecture/LectureBot.scala
@@ -9,8 +9,8 @@ class LectureBot extends Actor{
   val scalaBasic2: ActorRef = context.actorOf(Props[ScalaBasic2], name = "basic2")
   val scalaTests: ActorRef = context.actorOf(Props[ScalaTests], name = "tests")
   val functionalStyleAndMonads: ActorRef = context.actorOf(Props[FunctionalStyleAndMonads], name = "fucntionalStylasAndMonads")
-  //val scalaDsls : ActorRef = context.actorOf(Props[ScalaDSLs], name = "dsls")
-  //val scalaActors : ActorRef = context.actorOf(Props[ScalaActors], name = "actors")
+  val scalaDsls : ActorRef = context.actorOf(Props[ScalaDSLs], name = "dsls")
+  val scalaActors : ActorRef = context.actorOf(Props[ScalaActors], name = "actors")
   override def receive: Receive = {
     case msg:String if msg.equals("Basics") => scalaBasic1 ! msg
     case msg:String if msg.equals("more Basics") => scalaBasic2 ! msg
@@ -86,20 +86,63 @@ class FunctionalStyleAndMonads extends Actor{
   }
 }
 
-/*
+
 class ScalaDSLs extends Actor{
   val internalDSL: ActorRef = context.actorOf(Props[InternalDSL], name = "internalDSL")
-  val externalDSL: ActorRef = context.actorOf(Props[ExternaDSL], name = "externalDSL")
+  val externalDSL: ActorRef = context.actorOf(Props[ExternalDSL], name = "externalDSL")
+  override def receive: Receive = {
+    case msg:String if msg.equals("Internal DSL") => internalDSL ! msg
+    case msg:String if msg.equals("External DSL") => externalDSL ! msg
+    case _ =>
+  }
 }
 
 class ScalaActors extends Actor{
-
+  val log: LoggingAdapter = Logging(context.system, this)
+  var actors: String =
+    """
+      |Hello
+      |Here are the slides to Actors in Scala:
+      |https://docs.google.com/presentation/d/1xVV8AAPANPfufil4KLIp9uX3g6_tO7se9wGuCU7HT-M/edit#slide=id.g2fb348ba68_0_466
+      |Here is the video to Actors in Scala:
+      |https://drive.google.com/file/d/1TKm7F44ttuB1wJNbDvE9YzEXeiywjpom/view
+      |""".stripMargin
+  override def receive: Receive = {
+    case "Actors" => log.info(actors)
+    case _ =>
+  }
 }
 
 class InternalDSL extends Actor{
+  val log: LoggingAdapter = Logging(context.system, this)
+  var internalDSL: String =
+    """
+      |Hello
+      |Here are the slides to internal DSLs in Scala:
+      |https://docs.google.com/presentation/d/17qvdKHoIsxzfbcmF7Ay8VU_NGNayXsZjVsRmu6tcqug/edit#slide=id.g2fb348ba68_0_466
+      |Here is the video to internal DSLs in Scala:
+      |https://drive.google.com/file/d/1j_3oH5cj8aMKA0OZm6dDFVWVJfDwohyF/view
+      |""".stripMargin
 
+  override def receive: Receive = {
+    case "Internal DSL" => log.info(internalDSL)
+    case _ =>
+  }
 }
 
-class ExternaDSL extends Actor{
+class ExternalDSL extends Actor{
+  val log: LoggingAdapter = Logging(context.system, this)
+  var externalDSL: String =
+    """
+      |Hello
+      |Here are the slides to external DSLs in Scala:
+      |https://docs.google.com/presentation/d/1hZ73RtewIFgercCpan2EwT6fAv0ZmmQLMEUb4rUrSp8/edit#slide=id.g2fb348ba68_0_466
+      |Here is the video to external DSLs in Scala:
+      |https://drive.google.com/file/d/1GqdxtvDhzzNSubH42fQKRaPf987VScM1/view
+      |""".stripMargin
 
-}*/
+  override def receive: Receive = {
+    case "External DSL" => log.info(externalDSL)
+    case _ =>
+  }
+}
