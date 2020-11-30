00a63b865eaa8772235d9fe0842b3631cbe1e4e2
ScotSche <coding.scotsche@gmail.com>
2020-11-30 11:08:13 +0100

    Add Tests for CalculationBot

--- src/test/scala/CalculationBotSepc.scala
+++ src/test/scala/CalculationBotSepc.scala
@@ -22,15 +22,23 @@ class CalculationBotSpec()
     "return the answers for the following operators +" in{
       calculationBot ! "4+6"
       expectMsg("Result: 4 + 6 = 10")
+      calculationBot ! "4+1.5"
+      expectMsg("Result: 4 + 1.5 = 5.5")
 
       calculationBot ! "4-6"
       expectMsg("Result: 4 - 6 = -2")
+      calculationBot ! "4-1.5"
+      expectMsg("Result: 4 - 1.5 = 2.5")
 
       calculationBot ! "4*6"
       expectMsg("Result: 4 * 6 = 24")
+      calculationBot ! "4*1.5"
+      expectMsg("Result: 4 * 1.5 = 6")
 
       calculationBot ! "24/6"
       expectMsg("Result: 24 / 6 = 4")
+      calculationBot ! "1/2"
+      expectMsg("Result: 1 / 2 = 0.5")
     }
   }
 }
