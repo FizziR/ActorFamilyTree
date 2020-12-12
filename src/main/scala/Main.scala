d3c838bc6d7a4f0d776b178ae9c64daacead37e8
ScotSche <coding.scotsche@gmail.com>
2020-12-12 20:06:19 +0100

    Get (User, (AmountOfWords, AmountOfChars)) with Flows

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,9 @@ import akka.NotUsed
 import akka.actor.{ActorSystem, Props}
 import akka.stream.{ClosedShape, FlowShape}
 import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source, Zip}
+import shapeless.syntax.std.tuple.productTupleOps
 
+import scala.::
 import scala.collection.mutable.ListBuffer
 
 object Main{
@@ -23,19 +25,35 @@ object Main{
     val messageParserModel = new MessageParserModel()
     val convertsStringToMessage = builder.add(Flow[String].map(message => messageParserModel.generateMessageFromString(message)))
 
-    val output = builder.add(Sink.foreach[Option[Message]](println))
+    val convertOptionToMessage = builder.add(Flow[Option[Message]].map(message => message.getOrElse(Message(null, "", ""))))
+
+    val convertMessageToUser = builder.add(Flow[Message].map(message => message.user))
+    val convertMessageToWord = builder.add(Flow[Message].map(message => message.message.split(" ").length))
+    val convertMessageToChar = builder.add(Flow[Message].map(message => message.message.length))
 
 
-    //val broadcast = builder.add(Broadcast[String](2))
-    //val zip = builder.add(Zip[(String, String, String), String])
+    val output = builder.add(Sink.foreach[(String, (Int, Int))](println))
 
-    // step 3 - tying up the components
 
-    input ~> pasteSpaceBetweenArguments ~> convertsStringToMessage ~> output
+    val broadcast = builder.add(Broadcast[Message](2))
 
-    /*input ~> broadcast
-    broadcast.out(0) ~> splitStringsInTuples ~> zip.in0
-    zip.out ~> output*/
+    val wordBroadcast = builder.add(Broadcast[Message](2))
+    val wordZip = builder.add(Zip[Int, Int])
+
+    val zip = builder.add(Zip[String, (Int, Int)])
+
+    // step 3 - tying up the components*/
+
+    input ~> pasteSpaceBetweenArguments ~> convertsStringToMessage ~> convertOptionToMessage ~> broadcast
+
+    broadcast.out(0) ~> convertMessageToUser ~> zip.in0
+    broadcast.out(1) ~> wordBroadcast
+                        wordBroadcast.out(0) ~> convertMessageToWord ~> wordZip.in0
+                        wordBroadcast.out(1) ~> convertMessageToChar ~> wordZip.in1
+
+                        wordZip.out ~> zip.in1
+
+    zip.out ~> output
 
     ClosedShape
   }
