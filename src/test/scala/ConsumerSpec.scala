19495063ecc8a701d4d92a269d8fefcdae56a53a
FizziR <felicitas@maurerhaus.de>
2020-12-14 11:15:42 +0100

    Adding ConsumerSpec Class

--- src/test/scala/ConsumerSpec.scala
+++ src/test/scala/ConsumerSpec.scala
@@ -1,3 +1,23 @@
-class ConsumerSpec {
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import org.scalatest.wordspec.{AnyWordSpec, AnyWordSpecLike}
+
+class ConsumerSpec extends AnyWordSpec with Matchers {
+
+  val consumer = Consumer
+  val jsonString = s"""
+  {
+    "author": "Philip",
+    "words": 3,
+    "characters": 14
+  }
+  """.strip()
+
+  "The Consumer converts the recieving JsonFile to String" when{
+    "the following function is called" in{
+      //consumer.parseMessageToTUI(jsonString)
+
+    }
+  }
 
 }
