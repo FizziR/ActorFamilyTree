ce49740d92d4368525201a74cb4076992e6158a1
ScotSche <coding.scotsche@gmail.com>
2020-12-20 12:34:04 +0100

    Code Cleanup in SparkConsumer.scala

--- src/main/scala/Sparks/SparkConsumer.scala
+++ src/main/scala/Sparks/SparkConsumer.scala
@@ -17,9 +17,9 @@ import scala.compat.java8.FunctionConverters.enrichAsJavaFunction
 object SparkConsumer {
 
   var offset = 0
-  var oldValuesSumOfMessages = collection.mutable.Map[String, Long]()
-  var oldValueSumOfWords  = collection.mutable.Map[String, Long]()
-  var oldValuesSumOfChars = collection.mutable.Map[String, Long]()
+  var oldValuesSumOfMessages = collection.mutable.Map[String, Int]()
+  var oldValueSumOfWords  = collection.mutable.Map[String, Int]()
+  var oldValuesSumOfChars = collection.mutable.Map[String, Int]()
   def main(args: Array[String]) {
 
     val sparkConfig = new SparkConf().setMaster("local[*]").setAppName("DiscordStream")
@@ -37,7 +37,6 @@ object SparkConsumer {
 
     //val kafkaParams=kafkaConfig.asJava
     //val sc = new StreamingContext(spark.sparkContext, Seconds(1))
-
     val topics = Array("messagedata")
 
     val kafkaStream: InputDStream[ConsumerRecord[String, ProducerContent]] = KafkaUtils.createDirectStream[String, ProducerContent](
@@ -64,67 +63,40 @@ object SparkConsumer {
     discordData1Second.foreachRDD(rdd =>
     if(rdd.isEmpty()) println("Es gab keine neuen Daten...")
     else {
-
-
-      dataBase.show()
-
-      //println(rdd.toString())
+      //dataBase.show()
       dataBase = dataBase.union(rdd.toDF("Author", "Words", "Character"))
 
-      val x = dataBase.collect().length
-      println("Length: " + x)
-
-
-
-      /*val countDataFrame: DataFrame = spark.sql("select count(*) as total from dataFrame")
-        //countDataFrame.show()
-
-      val completeDataFrame = spark.sql("select * from dataFrame")
-        completeDataFrame.show()*/
-      println("Offset 1: " +offset)
-      val dataList = dataBase.collect().map(row => (row.getString(0), row.getInt(1), row.getInt(2))).toList
-
-      val dataListWithOffset= rdd.collect().map { case (author, wordCount, characterCount) => (author, wordCount, characterCount)}.toList
-      println("Length of dataList with offset: " +dataListWithOffset.length)
+      val dataListWithOffset = rdd.collect().map { case (author, wordCount, characterCount) => (author, wordCount, characterCount) }.toList
+      println("Length of dataList with offset: " + dataListWithOffset.length)
 
       val authorWithCounts = analyzingData.getUsersWhoWrote(sparkStreamingContext.sparkContext, dataListWithOffset)
-        println("Authors with count: "+authorWithCounts.mkString(""))
+      println("Authors with count: " + authorWithCounts.mkString(""))
 
       val authorWithSumOfWords = analyzingData.getSumOfWords(sparkStreamingContext.sparkContext, dataListWithOffset)
-        println("Authors with sum of words: " +authorWithSumOfWords.mkString(""))
+      println("Authors with sum of words: " + authorWithSumOfWords.mkString(""))
 
       val authorWithSumOfChars = analyzingData.getSumOfChars(sparkStreamingContext.sparkContext, dataListWithOffset)
-        println("Authors with sum of chars: " + authorWithSumOfChars.mkString(""))
-
-      offset = x
+      println("Authors with sum of chars: " + authorWithSumOfChars.mkString(""))
 
       authorWithCounts.map(authorWithMessages => {
-        if(oldValuesSumOfMessages.contains(authorWithMessages._1)) oldValuesSumOfMessages(authorWithMessages._1)+ authorWithMessages._2
-        else oldValuesSumOfMessages ++ authorWithMessages._1 -> authorWithMessages._2
+        if (oldValuesSumOfMessages.contains(authorWithMessages._1)) oldValuesSumOfMessages(authorWithMessages._1) += authorWithMessages._2
+        else oldValuesSumOfMessages += (authorWithMessages._1 -> authorWithMessages._2)
       })
       authorWithSumOfWords.map(authorWithWords => {
-        if(oldValueSumOfWords.contains(authorWithWords._1)) oldValueSumOfWords(authorWithWords._1)+ authorWithWords._2
-        else oldValueSumOfWords ++ authorWithWords._1 -> authorWithWords._2
+        if (oldValueSumOfWords.contains(authorWithWords._1)) oldValueSumOfWords(authorWithWords._1) += authorWithWords._2
+        else oldValueSumOfWords += (authorWithWords._1 -> authorWithWords._2)
       })
       authorWithSumOfChars.map(authorWithChars => {
-        if(oldValuesSumOfChars.contains(authorWithChars._1)) oldValuesSumOfChars(authorWithChars._1)+ authorWithChars._2
-        else oldValuesSumOfChars ++ authorWithChars._1 -> authorWithChars._2
+        if (oldValuesSumOfChars.contains(authorWithChars._1)) oldValuesSumOfChars(authorWithChars._1) += authorWithChars._2
+        else oldValuesSumOfChars += (authorWithChars._1 -> authorWithChars._2)
       })
 
-      println("Offset 2: " + offset)
-
       println("Map Count: " + oldValuesSumOfMessages + "\nMap Words: " + oldValueSumOfWords +
-      "\nMap Chars: " + oldValuesSumOfChars)
+        "\nMap Chars: " + oldValuesSumOfChars)
 
-      }
-
+    }
     )
-
     sparkStreamingContext.start()
     sparkStreamingContext.awaitTermination()
-
-
-
   }
-
 }
