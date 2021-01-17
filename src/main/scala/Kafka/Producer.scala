5fa392511bcc31d79a8e07734d3e61a9e1078eb0
ScotSche <73474321+ScotSche@users.noreply.github.com>
2021-01-17 13:53:16 +0100

    Update Producer.scala

--- src/main/scala/Kafka/Producer.scala
+++ src/main/scala/Kafka/Producer.scala
@@ -1,7 +1,8 @@
 package Kafka
 
 import java.util.Properties
-import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, RecordMetadata}
+import java.util.concurrent.Future
 
 class Producer {
 
@@ -24,5 +25,8 @@ class Producer {
     else{
       VALUES = VALUES.map(valuePair => if(valuePair.author.equals(producerInput.author)) ProducerContent(valuePair.author, valuePair.wordCount + producerInput.wordCount, valuePair.characterCount + producerInput.characterCount) else valuePair)
     }
+    val valueList: List[(String, Int, Int)] = VALUES.map(content => (content.author, content.wordCount, content.characterCount))
+    val message: ProducerRecord[String, ProducerContent] = new ProducerRecord(TOPIC_MESSAGEDATA, KEY_MESSAGEDATA, producerInput)
+    val result: Future[RecordMetadata] = producer.send(message)
   }
 }
