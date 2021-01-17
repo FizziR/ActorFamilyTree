c448903b27da3d1bb8f0055e38ad831d758b2b2d
ScotSche <coding.scotsche@gmail.com>
2021-01-17 14:44:34 +0100

    Make project runnable again without using Kafka

--- src/test/scala/ProducerSpec.scala
+++ src/test/scala/ProducerSpec.scala
@@ -5,11 +5,11 @@ import Kafka.Producer
 
 class ProducerSpec extends AnyWordSpec  with Matchers {
 
-  val feli = new ProducerContent("Feli", 10, 134)
-  val sebi = new ProducerContent("Sebi", 4, 20)
-  val philip = new ProducerContent("Philip", 5, 30)
+  val feli = ProducerContent("Feli", 10, 134)
+  val sebi = ProducerContent("Sebi", 4, 20)
+  val philip = ProducerContent("Philip", 5, 30)
   val values = List(feli, sebi, philip)
-  val newValue = new ProducerContent("Philip", 3, 14)
+  val newValue = ProducerContent("Philip", 3, 14)
 
   val producer = new Producer
   "In the producer the sumOfWords and sumOfCharacters is added to old values" when{
